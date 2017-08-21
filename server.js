df86cbdaaa52e950a9ead69d7bfa5e390a6a2454
Erick Ruiz <erick_ruiz23@yahoo.com>
2017-08-21 14:10:15 -0500

    fixed routing file

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ var express = require("express");
 var path = require("path");
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var route_setter = require('./server/config/routes.js');
+
+require('./server/config/mongoose.js');
 
 var app = express();
 
 app.use(bodyParser.json());
-
 app.use(express.static(path.join(__dirname, 'public/dist')));
-require('./server/config/mongoose.js');
 
-var route_setter = require('./server/config/routes.js');
 route_setter(app);
 
 var server = app.listen(8000, function () {
