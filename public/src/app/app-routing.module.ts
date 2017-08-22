dcc193b6cda1dffb5cbb6a9413e3489fa91327a8
Erick Ruiz <erick_ruiz23@yahoo.com>
2017-08-22 11:44:39 -0500

    finished login/reg

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LogregComponent } from './logreg/logreg.component';
+import { HomeComponent } from './home/home.component';
+
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: LogregComponent },
+  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
--- public/src/app/logreg/logreg.component.ts
+++ public/src/app/logreg/logreg.component.ts
@@ -11,72 +11,15 @@ import { PinService } from '../pin.service';
 })
 export class LogregComponent implements OnInit {
 
-  users
-  // user = new User()  
-  exists = false;
-  registered = false;
-  pwError = false;
-  emailError = false;
-  onlyPW = false;
-  login;
-
-  constructor(private _pinService:PinService, private _router: Router){}
-
-
-  ngOnInit(){
-    this.getUsers();
-    this.login = {email: '', password: ''}
-  }
-  
-  getUsers() {
-    this._pinService.retrieveUsers()
-      .then(users => this.users = users)
-      .catch(err => console.log(err));
-  }
-
-  // createUser(){
-  //   this.exists = false;
-  //   for (let i in this.users) {
-  //     if (this.users[i].email == this.user.email){
-  //       this.exists = true;
-  //       this.user = new User();
-  //     }
-  //   }
-  //   if (!this.exists) {
-  //     this._pinService.sendUser(this.user).then(response => console.log (response)).catch(err => console.log(err));
-  //     this.user = new User();
-  //     this.getUsers();
-  //   }
-  // }
-
-  loginUser(){
-    this.registered = false;
-    this.pwError = false;
-    this.emailError = false;
-    this.onlyPW = false;
-    for (let i in this.users) {
-      if (this.users[i].email == this.login.email){
-        if (this.users[i].password == this.login.password) {
-          this.registered = true;
-        } else {
-          this.pwError = true;
-          this.onlyPW = true;
-        }
-      }
-    }
-    if (this.registered){
-      this._pinService.login(this.login).then(response => {
-        this._router.navigateByUrl('/dashboard');
-      }).catch(err => console.log(err));
-    } else if (this.onlyPW) {
-      this.emailError = false;
-    } else {
-      this.emailError = true;
-    }
-  }
-
+  exists = false
+  logForm
   gender = false
   register = true
+  valid = false
+  currentUser
+  validUser
+
+
   user = {
     email: '',
     password: '',
@@ -84,22 +27,56 @@ export class LogregComponent implements OnInit {
     gender: '',
     age: ''
   }
-  valid = []
-
-  onSubmit(){
-    this.gender = true
-    this.register = false
-    
+  login = {
+    email: '',
+    password: ''
   }
+
+  constructor(private _pinService:PinService, private _router: Router){}
+
+
+  ngOnInit(){
+
+  }
+  
+  onSubmit(){
+    this._pinService.checkEmail(this.user).then(apiUser => {
+      if(apiUser.length >=1){
+        this.exists = true
+        this.gender = false
+        this.register = true   
+      }
+      else{
+        this.gender = true
+        this.register = false   
+      }
+    }).catch(err => console.log(err));
+  }
+
+  addUser(){
+    this._pinService.sendUser(this.user).then(user => this.validUser = user).catch(err => console.log(err));    
+  }
+
+  loginForm(){
+    this.gender = false
+    this.register = false
+    this.logForm = true
+  }
+
+  loginUser(){
+      this._pinService.login(this.login).then(apiUser => {
+        this.currentUser = apiUser;        
+        if(apiUser.length >=1){
+          this._router.navigateByUrl('/home');
+        }
+        else{
+          this.valid = true;
+        }
+      }).catch(err => console.log(err));
+    }
 
   goBack(){
     this.gender = false
     this.register = true
   }
-
-  addUser(){
-    console.log(this.user)
-    this._pinService.sendUser(this.user).then(user => this.valid = user).catch(err => console.log(err));    
-  }
-
 }
